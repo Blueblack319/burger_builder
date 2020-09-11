e8110b13df6b41acdf862b1eb08e4c24cdb3c2b2
Jae Hoon Yang <didwogns789@naver.com>
2020-09-11 16:00:01 +0900

    Sending a POST Request to firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,8 @@ import BuildControls from "../../components/Burger/BuildContols/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
 
+import axios from "../../axios-orders";
+
 const INGREDIENT_PRICE = {
   salad: 0.5,
   cheese: 0.4,
@@ -77,6 +79,27 @@ class BurgerBuilder extends Component {
   };
 
   handleContinueOrder = () => {
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Crazybirdz",
+        address: {
+          street: "TestStreet 1",
+          zipCode: "32543",
+          country: "South Korea",
+        },
+        email: "test@test.com",
+      },
+      deliveryMode: "fastest",
+    };
+    axios({
+      method: "post",
+      url: "orders.json",
+      data: order,
+    })
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
     alert("Your Order Continue!!");
   };
 
@@ -97,7 +120,7 @@ class BurgerBuilder extends Component {
             cancleOrder={this.handleCancleOrder}
             continueOrder={this.handleContinueOrder}
             ingredients={this.state.ingredients}
-            price={this.state.totalPrice.toFixed(2)}
+            price={parseInt(this.state.totalPrice.toFixed(2))}
           />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
