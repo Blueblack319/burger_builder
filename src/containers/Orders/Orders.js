fd340f0885f5a485efe9929a2d83907b6e6a923d
Yang Jae Hoon <didwongs789@naver.com>
2020-09-25 16:47:56 +0900

    Fetching Orders (via Redux)

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -4,6 +4,7 @@ import Order from "../../components/Order/Order";
 
 import { connect } from "react-redux";
 import * as actionCreators from "../../store/actions/index";
+import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
   state = {
@@ -16,15 +17,18 @@ class Orders extends Component {
   }
 
   render() {
-    const orders = this.props.orders.map((order) => {
-      return (
-        <Order
-          key={order.id}
-          ingredients={order.ingredients}
-          price={order.price}
-        />
-      );
-    });
+    let orders = <Spinner />;
+    if (!this.props.loading) {
+      orders = this.props.orders.map((order) => {
+        return (
+          <Order
+            key={order.id}
+            ingredients={order.ingredients}
+            price={order.price}
+          />
+        );
+      });
+    }
     return <div>{orders}</div>;
   }
 }
