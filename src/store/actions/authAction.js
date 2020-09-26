933d16014cbe8978705f44c08b26a07872222b60
Yang Jae Hoon <didwongs789@naver.com>
2020-09-26 12:56:16 +0900

    Getting a Token from the Backend

--- src/store/actions/authAction.js
+++ src/store/actions/authAction.js
@@ -1,6 +1,8 @@
 import actionTypes from "./actionTypes";
 import axios from "../../axios-orders";
 
+const API_KEY = "AIzaSyBpOCU_QrvEE_-dIPMC97gFJalvsEFZGGc";
+
 const authSuccess = (authData) => {
   return {
     type: actionTypes.AUTH_SUCCESS,
@@ -24,18 +26,22 @@ const authStart = () => {
 export const auth = (email, password) => {
   return (dispatch) => {
     dispatch(authStart());
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true,
+    };
     axios({
       method: "post",
-      url: "auth.json",
+      url: `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+      data: authData,
     })
       .then((res) => {
-        const authData = {
-          email,
-          password,
-        };
-        dispatch(authSuccess(authData));
+        console.log(res);
+        dispatch(authSuccess(res.data));
       })
       .catch((error) => {
+        console.log(error);
         dispatch(authFail(error));
       });
   };
